7367d46767344c14aaad496f1bb4a502c9159cf4
harshkachhaway <harshkachhaway8001@gmail.com>
2023-09-20 00:18:57 +0530

    made some changes to counter in javascript

--- counter/index.js
+++ counter/index.js
@@ -2,6 +2,8 @@ let count = document.querySelector("#count")
 let decrese = document.querySelector("#decrese")
 let reset = document.querySelector("#reset")
 let increse = document.querySelector("#increse")
+let body = document.querySelector("body")
+
 let num = 0;
 
 
@@ -28,13 +30,17 @@ function color(){
     if (num > 0) {
         // document.querySelector("count").style.color = "blue6"
         // console.log("he")
-        count.style.color = "blue"
+        count.style.color = "rgb(65, 65, 223)"
+        body.style.background = "rgb(65, 65, 223)"
+        
     }
     else if (num < 0 ){
-        count.style.color = "red"
-
+        count.style.color = "rgb(228, 57, 57)"
+        body.style.background = "rgb(228, 57, 57)"
+        
     }
     else{
+        body.style.background = "rgba(0, 0, 0, 0.589)"
         count.style.color = "white"
 
     }
